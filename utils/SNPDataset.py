2d9af2fb0adb8f1533d2ea0eaaffa0948ddccb51
pikruse <pikruser@gmail.com>
2024-11-12 13:00:08 -0500

    added SNP dataset object

--- utils/SNPDataset.py
+++ utils/SNPDataset.py
@@ -18,12 +18,19 @@ from transformers.models.bert.configuration_bert import BertConfig
 
 class SNPDataset(Dataset):
 
-    def __init__(self, data, tokenizer):
+    def __init__(self, 
+                 data, 
+                 tokenizer):
+        """
+        Parameters:
+            data (): SNP dataset to use 
+            tokenizer (hf tokenizer): tokenizer to use
+        """
+
         # NOTE: not sure what our input data will look like yet... 
         # probably want to make it a numpy array or tensor or something
-        
-
-        pass
+        self.data = data
+        self.tokenizer = tokenizer
 
     def __len__(self):
         # return length (number of rows) in dataset
@@ -37,6 +44,8 @@ class SNPDataset(Dataset):
             tokens (torch.tensor): tokenized SNP sequence
             attention_mask (torch.tensor): attention mask for tokenized SNP sequence
         """
-
-
+        inputs = self.tokenizer(self.data[index], return_tensors="pt")
+        tokens = inputs["input_ids"]
+        attention_mask = inputs["attention_mask"]
+        
         return tokens, attention_mask
