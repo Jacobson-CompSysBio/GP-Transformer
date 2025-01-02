2bbbf8c5e85911614ac18980e917e005e18fa6ce
pikruse <pikruser@gmail.com>
2025-01-02 15:45:09 -0500

    add g, e datasets

--- utils/Dataset.py
+++ utils/Dataset.py
@@ -64,3 +64,74 @@ class GxEDataset(Dataset):
         env_data = torch.tensor(self.e_data[index].values, dtype=torch.float32)
         
         return tokens, attention_mask, env_data
+
+# only genotype data
+class GDataset(Dataset):
+
+    def __init__(self, 
+                 data_path, 
+                 tokenizer):
+        """
+        Parameters:
+            data_path (str): dataset to use 
+            tokenizer (hf tokenizer): tokenizer to use for markers
+        """
+
+        # NOTE: not sure what our input data will look like yet... 
+        # probably want to make it a numpy array or tensor or something
+        self.g_data = pd.read_csv(data_path).iloc[:, :2240]
+
+        # get tokenizer ready
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        # return length (number of rows) in dataset
+        return len(self.g_data)
+    
+    def __getitem__(self, index):
+        """
+        Parameters
+            index (int): index to return data from
+        Returns:
+            tokens (torch.tensor): tokenized SNP sequence
+            attention_mask (torch.tensor): attention mask for tokenized SNP sequence
+        """
+
+        # get genotype data
+        inputs = self.tokenizer(self.g_data[index], return_tensors="pt")
+        tokens = inputs["input_ids"]
+        attention_mask = inputs["attention_mask"]
+
+        return tokens, attention_mask
+    
+# only environmental data
+class EDataset(Dataset):
+
+    def __init__(self, 
+                 data_path, 
+                 tokenizer):
+        """
+        Parameters:
+            data_path (str): dataset to use 
+            tokenizer (hf tokenizer): tokenizer to use for markers
+        """
+
+        # NOTE: not sure what our input data will look like yet... 
+        # probably want to make it a numpy array or tensor or something
+        self.e_data = pd.read_csv(data_path).iloc[:, 2240:] 
+
+    def __len__(self):
+        # return length (number of rows) in dataset
+        return len(self.e_data)
+    
+    def __getitem__(self, index):
+        """
+        Parameters
+            index (int): index to return data from
+        Returns:
+            env_data (torch.tensor): environmental data observation
+        """
+        # get env data
+        env_data = torch.tensor(self.e_data[index].values, dtype=torch.float32)
+        
+        return env_data
