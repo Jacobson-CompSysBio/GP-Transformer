48d2ecb5a68f5c963db21271cd483915828ef667
pikruse <pikruser@gmail.com>
2025-01-03 10:53:31 -0500

    changed dataset

--- utils/Dataset.py
+++ utils/Dataset.py
@@ -19,24 +19,36 @@ from transformers.models.bert.configuration_bert import BertConfig
 
 class GxEDataset(Dataset):
 
-    def __init__(self, 
-                 data_path, 
-                 tokenizer):
+    def __init__(self,
+                 tokenizer, 
+                 split='train'
+                 ):
         """
         Parameters:
-            data_path (str): dataset to use 
+            split (str): train, val, or test
             tokenizer (hf tokenizer): tokenizer to use for markers
         """
 
-        # NOTE: not sure what our input data will look like yet... 
-        # probably want to make it a numpy array or tensor or something
-        self.data = pd.read_csv(data_path)
+        # load data depending on split
+        if split == 'train':
+            x_path = '../data/position_ec_raw_genotype/X_train.csv'
+            y_path = '../data/position_ec_raw_genotype/y_train.csv'
+        elif split == 'val':
+            x_path = '../data/position_ec_raw_genotype/X_val.csv'
+            y_path = '../data/position_ec_raw_genotype/y_val.csv'
+        else:
+            x_path = '../data/position_ec_raw_genotype/X_test.csv'
+            y_path = '../data/position_ec_raw_genotype/y_test.csv'
+
+        # load data
+        self.x_data = pd.read_csv(x_path, index_col=0).reset_index() # reset index col
+        self.y_data = pd.read_csv(y_path, index_col=0).reset_index()
 
         # first 2240 features are genotype data
-        self.g_data = self.data.iloc[:, :2240]
+        self.g_data = self.x_data.iloc[:, :2240] # don't need first column
 
         # last 2240 features are lat/long and EC data
-        self.e_data = self.data.iloc[:, 2240:] 
+        self.e_data = self.x_data.iloc[:, 2240:] 
 
         # get tokenizer ready
         self.tokenizer = tokenizer
@@ -61,9 +73,12 @@ class GxEDataset(Dataset):
         attention_mask = inputs["attention_mask"]
 
         # get env data
-        env_data = torch.tensor(self.e_data[index].values, dtype=torch.float32)
+        env_data = torch.tensor(self.e_data.values, dtype=torch.float32)
+
+        inp = {'tokens': tokens, 'attention_mask': attention_mask, 'env_data': env_data}
+        target = {'target': torch.tensor(self.y_data[index].values, dtype=torch.float32)}
         
-        return tokens, attention_mask, env_data
+        return inp, target 
 
 # only genotype data
 class GDataset(Dataset):
