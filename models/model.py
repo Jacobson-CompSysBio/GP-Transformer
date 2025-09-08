ab6d0906d52de862fdb9878ac6126769ff8823f2
Peter Kruse <peter.ingraham.kruse@gmail.com>
2025-09-08 13:30:50 -0400

    added granular control for each layer in config

--- models/model.py
+++ models/model.py
@@ -89,3 +89,82 @@ class GxE_Transformer(nn.Module):
 
         return self.final_layer(x)
 
+# ----------------------------------------------------------------
+# create full GxE transformer for genomic prediction
+class GxE_Transformer_Extended(nn.Module):
+
+    """
+    """
+    
+    def __init__(self,
+                 g_enc: bool = True,
+                 e_enc: bool = True,
+                 ld_enc: bool = True,
+                 final_tf: bool = True,
+                 config = None
+                 ):
+        super().__init__()
+
+        self.config = config
+
+        # set attributes
+        self.g_encoder = G_Encoder(config) if g_enc else None
+        self.e_encoder = E_Encoder(output_dim=config.n_embd,
+                                   n_hidden=config.n_mlp_layer,
+                                   dropout=config.dropout) if e_enc else None
+        self.ld_encoder = LD_Encoder(input_dim=config.vocab_size,
+                                     output_dim=config.n_embd,
+                                     num_blocks=config.n_mlp_layer,
+                                     dropout=config.dropout) if ld_enc else None
+        if final_tf:
+            self.final_tf = True
+            self.hidden_layers = nn.ModuleList(
+            [TransformerBlock(config) for _ in range(config.n_gxe_layer)]
+            + [nn.LayerNorm(config.n_embd)]
+            )
+        else:
+            self.final_tf = False
+            self.hidden_layers = nn.ModuleList(
+                [Block(self.g_encoder.output_dim if i == 0 else config.n_embd,
+                    config.n_embd,
+                    dropout=config.dropout,
+                    activation=nn.GELU(),
+                    ) for i in range(config.n_gxe_layer)]
+            )
+        
+        # init final layer (output of 1 for regression)
+        self.final_layer = nn.Linear(config.n_embd, 1) # CAN CHANGE INPUT, OUTPUT SIZE FOR LAYERS
+
+    def _forward_tf(self, g_enc, e_enc, ld_enc):
+        if isinstance(e_enc, torch.Tensor): 
+            e_enc = e_enc.unsqueeze(dim=1)
+        x = g_enc + e_enc + ld_enc
+        for layer in self.hidden_layers:
+            x = layer(x)
+        x = x.mean(dim=1) # (B, T, n_embd) -> (B, n_embd)
+        return x
+
+    def _forward_mlp(self, g_enc, e_enc, ld_enc):
+        # convert [B, T, C] -> [B, C]
+        if isinstance(g_enc, torch.Tensor):
+            g_enc = g_enc.mean(dim=1)
+        if isinstance(ld_enc, torch.Tensor):
+            ld_enc = ld_enc.mean(dim=1)
+        x = g_enc + e_enc + ld_enc
+        for layer in self.hidden_layers:
+            x = x + layer(x)
+        return x
+    
+    def forward(self, x):
+        g_enc = self.g_encoder(x["g_data"]) if self.g_encoder else 0
+        e_enc = self.e_encoder(x["e_data"]) if self.e_encoder else 0
+        ld_enc = 0
+        if self.ld_encoder:
+            ld_feats = F.one_hot(x["g_data"].long(), num_classes=self.ld_encoder.input_dim)
+            ld_enc = self.ld_encoder(ld_feats.float())
+        if self.final_tf:
+            x = self._forward_tf(g_enc, e_enc, ld_enc)
+        else:
+            x = self._forward_mlp(g_enc, e_enc, ld_enc)
+
+        return self.final_layer(x)
