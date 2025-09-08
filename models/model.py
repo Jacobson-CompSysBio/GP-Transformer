7a63e2f5df237e7920541b7592063aa2f6f96878
Peter Kruse <peter.ingraham.kruse@gmail.com>
2025-09-08 16:52:22 -0400

    setting up final cnn block for model

--- models/model.py
+++ models/model.py
@@ -22,7 +22,7 @@ class GxE_Transformer(nn.Module):
                  g_enc: bool = True,
                  e_enc: bool = True,
                  ld_enc: bool = True,
-                 final_tf: bool = True,
+                 gxe_enc: bool = True,
                  moe: bool = True,
                  config = None
                  ):
@@ -40,14 +40,14 @@ class GxE_Transformer(nn.Module):
                                      num_blocks=config.n_ld_layer,
                                      dropout=config.dropout) if ld_enc else None
         self.moe_w = nn.Parameter(torch.tensor([1.0, 1.0, 1.0])) if moe else None
-        if final_tf:
-            self.final_tf = True
+        if gxe_enc == "tf":
+            self.gxe_enc = "tf"
             self.hidden_layers = nn.ModuleList(
             [TransformerBlock(config) for _ in range(config.n_gxe_layer)]
             + [nn.LayerNorm(config.n_embd)]
             )
-        else:
-            self.final_tf = False
+        elif gxe_enc == "mlp":
+            self.gxe_enc = "mlp"
             self.hidden_layers = nn.ModuleList(
                 [Block(self.g_encoder.output_dim if i == 0 else config.n_embd,
                     config.n_embd,
@@ -55,6 +55,12 @@ class GxE_Transformer(nn.Module):
                     activation=nn.GELU(),
                     ) for i in range(config.n_gxe_layer)]
             )
+        elif gxe_enc == "cnn":
+            self.gxe_enc = "cnn"
+            # write cnn code here
+        else:
+            raise ValueError("gxe_enc must be one of ['tf', 'mlp', 'cnn']")
+
         
         # init final layer (output of 1 for regression)
         self.final_layer = nn.Linear(config.n_embd, 1) # CAN CHANGE INPUT, OUTPUT SIZE FOR LAYERS
@@ -86,6 +92,15 @@ class GxE_Transformer(nn.Module):
             x = x + layer(x)
         return x
     
+    def _forward_cnn(self, g_enc, e_enc, ld_enc):
+        if isinstance(e_enc, torch.Tensor): 
+            e_enc = e_enc.unsqueeze(dim=1)
+        x = self._concat(g_enc, e_enc, ld_enc)
+        for layer in self.hidden_layers:
+            x = x + layer(x)
+        x = x.mean(dim=1) # (B, T, n_embd) -> (B, n_embd) 
+        return x
+    
     def forward(self, x):
         g_enc = self.g_encoder(x["g_data"]) if self.g_encoder else 0
         e_enc = self.e_encoder(x["e_data"]) if self.e_encoder else 0
@@ -93,9 +108,13 @@ class GxE_Transformer(nn.Module):
         if self.ld_encoder:
             ld_feats = F.one_hot(x["g_data"].long(), num_classes=self.ld_encoder.input_dim)
             ld_enc = self.ld_encoder(ld_feats.float())
-        if self.final_tf:
+        if self.gxe_enc == "tf":
             x = self._forward_tf(g_enc, e_enc, ld_enc)
-        else:
+        elif self.gxe_enc == "mlp":
             x = self._forward_mlp(g_enc, e_enc, ld_enc)
+        elif self.gxe_enc == "cnn":
+            x = self._forward_cnn(g_enc, e_enc, ld_enc)
+        else:
+            raise ValueError("gxe_enc must be one of ['tf', 'mlp', 'cnn']")
 
         return self.final_layer(x)
